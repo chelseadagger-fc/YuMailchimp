542cbcd424f04dd545907e2f996221e780e50046
Daniel <daniel.aldridge@alumni.unc.edu>
2023-06-05 03:12:41 +0300

    finished setup

--- app.js
+++ app.js
@@ -5,9 +5,26 @@ const https = require("https");
 // NOTES: use 1. https instead of request
 //            2. app.use(express.json()); instead of app.use(bodyParser());
 
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get("/", function(req,res) {
+    
+    res.sendFile(__dirname + "/signup.html")
+
+})
 
 
 
+app.post("/", function (req,res) {
+
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const emailAddress = req.body.email;
+    console.log(firstName, lastName, emailAddress);
+
+})
 
 
 
